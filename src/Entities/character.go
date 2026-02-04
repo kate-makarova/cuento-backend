934dc935b24c2f905f1f528e276a16d5f846585f
Kate Makarova <kate.makarova@hotmail.com>
2026-02-04 07:30:52 -0500

    character CRUD

--- src/Entities/character.go
+++ src/Entities/character.go
@@ -10,6 +10,6 @@ type Character struct {
 }
 
 func GenerateCharacterTable(db *sql.DB) {
-	sql := "CREATE TABLE characters (id SERIAL, name VARCHAR(255), avatar VARCHAR(255))"
+	sql := "CREATE TABLE character_base (id SERIAL, name VARCHAR(255), avatar VARCHAR(255))"
 	db.Exec(sql)
 }
